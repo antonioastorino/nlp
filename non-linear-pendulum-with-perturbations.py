ec071c144b3eda86eef992556f1c063317a8736b
Antonio Astorino <antonio.astorino.phd@gmail.com>
2025-03-01 11:24:16 +0100

    Fix definition of damping coefficient

--- non-linear-pendulum-with-perturbations.py
+++ non-linear-pendulum-with-perturbations.py
@@ -11,6 +11,7 @@ during the first N time steps
 
 '''
 
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -24,7 +25,7 @@ l = 0.5  # m
 m = 0.5  # kg
 # forced_torque_vec = [70 for _ in range(0, 60)]
 forced_torque_vec = [2 * np.sin(np.sqrt(g / l) * i * dt) for i in range(0, 4000)]
-damping_k = 400
+damping_k = 0.8  # kg / s^2
 #################################### End of parameters ############################################
 
 NUM_OF_SAMPLES = round(DURATION / dt)
@@ -40,41 +41,44 @@ def myPlotter(xVals, yVals, xLabel, yLabel, title):
 
 
 def theta_t_plus_dt(dt, theta_t, theta_t_minus_dt, forced_torque=0, damping_k=0):
-    alpha_f = damping_k * (theta_t_minus_dt - theta_t) / m
+    alpha_f = damping_k * (theta_t_minus_dt - theta_t) / dt / m
     alpha_in = forced_torque / l / l / m
     alpha_g = -g / l * np.sin(theta_t)
     alpha = alpha_g + alpha_in + alpha_f
     return alpha * (dt * dt) - theta_t_minus_dt + 2 * theta_t
 
 
-t = np.linspace(0, NUM_OF_SAMPLES * dt, NUM_OF_SAMPLES - 1)
+t = np.linspace(0, NUM_OF_SAMPLES * dt, NUM_OF_SAMPLES + 1)
 theta_curr = 0
 theta_prev = 0
 theta_next = 0
-theta_vec = [0 for _ in range(1, NUM_OF_SAMPLES)]
-omega_vec = [0 for _ in range(1, NUM_OF_SAMPLES)]
-torque_vec = [0 for _ in range(1, NUM_OF_SAMPLES)]
-len_of_forced_torque_vec = min(NUM_OF_SAMPLES - 1, len(forced_torque_vec))
+theta_vec = [0 for _ in range(0, NUM_OF_SAMPLES + 1)]
+omega_vec = [0 for _ in range(0, NUM_OF_SAMPLES + 1)]
+torque_vec = [0 for _ in range(0, NUM_OF_SAMPLES + 1)]
+len_of_forced_torque_vec = min(NUM_OF_SAMPLES + 1, len(forced_torque_vec))
 
 # If there is any user-defined torque, this will be used as a system input
 for i in range(0, len_of_forced_torque_vec):
     torque_vec[i] = forced_torque_vec[i]
 
-for i in range(0, NUM_OF_SAMPLES - 1):
+
+start_time = time.time()
+for i in range(0, NUM_OF_SAMPLES + 1):
     theta_next = theta_t_plus_dt(dt, theta_curr, theta_prev, torque_vec[i], damping_k)
     omega_vec[i] = (theta_next - theta_curr) / dt
     theta_prev = theta_curr
     theta_curr = theta_next
     theta_vec[i] = theta_curr
+print("Computation time =", time.time() - start_time)
 
 myPlotter(t, theta_vec, 't [s]', 'theta [rad]', 'angle vs time')
 myPlotter(t, torque_vec, 't [s]', 'applied torque [Nm]', 'Applied torque')
 
 # Energy / state space
 myPlotter(theta_vec, omega_vec, 'theta [rad]', 'omega [rad/s]', 'State Space')
-KE = [(omega_vec[i] * l)**2 * m / 2 for i in range(0, NUM_OF_SAMPLES - 1)]
-U = [m * g * l * (1 - np.cos(theta_vec[i])) for i in range(0, NUM_OF_SAMPLES - 1)]
-E = [KE[i] + U[i] for i in range(0, NUM_OF_SAMPLES - 1)]
+KE = [(omega_vec[i] * l)**2 * m / 2 for i in range(0, NUM_OF_SAMPLES + 1)]
+U = [m * g * l * (1 - np.cos(theta_vec[i])) for i in range(0, NUM_OF_SAMPLES + 1)]
+E = [KE[i] + U[i] for i in range(0, NUM_OF_SAMPLES + 1)]
 myPlotter(KE, U, 'kinetic [J]', 'potential [J]', 'Energy')
 myPlotter(t, E, 'time [s]', 'total energy [J]', 'Energy vs time')
 
@@ -86,7 +90,7 @@ if (animation):
     position_plot, = ax.plot(0, 0, marker='*')
     refresh_rate = 50  # Hz
     undersampling_rate = round(1 / dt / refresh_rate)
-    for i in range(0, NUM_OF_SAMPLES - 1, undersampling_rate):
+    for i in range(0, NUM_OF_SAMPLES + 1, undersampling_rate):
         position_plot.set_data([0, l * np.sin(theta_vec[i])], [0, - l * np.cos(theta_vec[i])])
         plt.pause(1 / refresh_rate)
 
